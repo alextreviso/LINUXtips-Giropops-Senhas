5f684d7aa121eca440d9071ea7a1344fb94b2985
Alex Treviso <alextreviso12@gmail.com>
2024-05-04 20:32:40 -0300

    add script to run app

---
+++ scripts/deploy-app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker container run -d --name redis -p 6379:6379 redis:4 
+
+REDIS_HOST=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' redis)
+
+docker image build -t alextreviso/linuxtips-giropops-senha:1.0 --build-arg REDIS_HOST=$REDIS_HOST .
+
+docker container run -d -p 5000:5000 alextreviso/linuxtips-giropops-senha:1.0

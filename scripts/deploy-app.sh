fbfb40bf125a30ef37cdd1f0d2e0a0c79b61923e
Alex Treviso <alextreviso12@gmail.com>
2024-05-04 20:55:08 -0300

    add container name

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -6,4 +6,4 @@ REDIS_HOST=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}
 
 docker image build -t alextreviso/linuxtips-giropops-senha:1.0 --build-arg REDIS_HOST=$REDIS_HOST .
 
-docker container run -d -p 5000:5000 alextreviso/linuxtips-giropops-senha:1.0
+docker container run -d -p 5000:5000 --name giropops-senhas alextreviso/linuxtips-giropops-senha:1.0
